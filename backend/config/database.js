f8e9861d855cf5564d1a802da6f7902af23478db
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-04 19:53:51 +0500

    DB Connection

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+module.exports = connectDatabase;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,14 @@
 const app = require("./app");
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
 // Config
 
 dotenv.config({ path: "backend/config/config.env" });
+
+// Connecting database
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is workin on http://localhost:${process.env.PORT}`);
