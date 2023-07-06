c48de295df04fd31dad621166d3db14ad066b41f
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-06 23:53:46 +0500

    userModel

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -5,5 +5,19 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     require: [true, "Please Enter Your Name"],
+    maxLength: [30, "Name cannot exceed 30 characters"],
+    minLength: [4, "Name should have more than 4 characters"],
+  },
+  email: {
+    type: String,
+    require: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter a valid Email"],
+  },
+  password: {
+    type: String,
+    require: [true, "Please Enter Your Password"],
+    minLength: [8, "Password should be grater than 8 characters"],
+    select: false,
   },
 });
