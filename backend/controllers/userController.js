c23f70215d6387977a7850c3233e75f5208db0d1
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-07 20:23:16 +0500

    userRegistrationModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -17,8 +17,12 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
     },
   });
 
+  const token = user.getJWTToken();
+
   res.status(200).json({
     success: true,
-    user,
+    token,
   });
 });
+
+// Login User
--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,5 +39,20 @@ const userSchema = new mongoose.Schema({
   resetPasswordToken: String,
   resetPasswordExpire: Date,
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+// JWT TOKEN
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model("User", userSchema);
