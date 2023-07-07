a7c708b051a7b62ce7d6304d328ef04b88e8acfb
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-08 01:39:57 +0500

    userModel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -19,10 +19,39 @@ exports.registerUser = catchAsyncError(async (req, res, next) => {
 
   const token = user.getJWTToken();
 
-  res.status(200).json({
+  res.status(201).json({
     success: true,
     token,
   });
 });
 
 // Login User
+
+exports.loginUser = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Checking if user given email and password both
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please Enter Your Email & Password", 400));
+  }
+
+  const user = User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+
+  const isPasswordMatched = user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password"), 401);
+  }
+
+  const token = user.getJWTToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
