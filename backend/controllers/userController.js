781e8c36e2fbe33c8ef9c82e77e01bcc4bfca144
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-11 01:53:33 +0500

    admin routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -218,3 +218,41 @@ exports.getSingleUser = catchAsyncError(async (req, res, next) => {
     user,
   });
 });
+
+// Update User Role - Admin
+exports.updateProfile = catchAsyncError(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// Delete User - Admin
+exports.deleteUser = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  // We will remove cloudinary
+
+  if (!user) {
+    return next(
+      new ErrorHandler(`User does not exist with Id: ${req.params.id}`)
+    );
+  }
+
+  await user.remove();
+
+  res.status(200).json({
+    success: true,
+  });
+});
