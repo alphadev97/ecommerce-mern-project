7972295573e1a4da7f830d6b2e9ad96f731086f7
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-10 23:57:35 +0500

    user details

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -139,3 +139,13 @@ exports.resetPassword = catchAsyncError(async (req, res, next) => {
 
   sendToken(user, 200, res);
 });
+
+// Get User Detail
+exports.getUserDetails = catchAsyncError(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
