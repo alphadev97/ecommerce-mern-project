7aea9d14913bdbfef1650b7711df4dad7dce8a54
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-12 20:18:25 +0500

    frontend update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
-import "./App.css";
-import Header from "./component/layout/Header.js";
-import { BrowserRouter as Router } from "react-router-dom";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Header />
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- frontend/src/component/layout/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ReactNavbar } from "overlay-navbar";
-
-const Header = () => {
-  return <ReactNavbar />;
-};
-
-export default Header;
