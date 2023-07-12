d14c596fe880c6326b3aede5160098bb8a92421a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-12 17:38:15 +0500

    haeder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import "./App.css";
 import Header from "./component/layout/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
-  return <Header />;
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
 }
 
 export default App;
---
+++ frontend/src/component/layout/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactNavbar } from "overlay-navbar";
+
+const Header = () => {
+  return <ReactNavbar />;
+};
+
+export default Header;
