8fe24ffb5b182a38bd4e5ec38b962d22fd8d35a5
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-14 01:25:12 +0500

    home

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -1,6 +1,18 @@
 import React, { Fragment } from "react";
 import { CgMouse } from "react-icons/cg";
 import "./Home.scss";
+import Product from "./Product.js";
+
+const product = {
+  name: "Blue Shirt",
+  images: [
+    {
+      url: "https://i5.walmartimages.com/asr/3c92c819-468f-4edd-8098-551a16ea9f1c.a8809b55a1038c8f51139443cd9e92db.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    },
+  ],
+  price: "Rs.3000",
+  _id: "alpha97",
+};
 
 const Home = () => {
   return (
@@ -14,6 +26,12 @@ const Home = () => {
             Scroll <CgMouse />
           </button>
         </a>
+      </div>
+
+      <h2 className="homeHeading">Featured Product</h2>
+
+      <div className="container" id="container">
+        <Product product={product} />
       </div>
     </Fragment>
   );
---
+++ frontend/src/component/Home/Product.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import ReactStars from "react-rating-stars-component";
+
+const options = {
+  edit: false,
+  color: "rgba(20,20,20,0.1)",
+  activeColor: "tomato",
+  size: window.innerWidth < 600 ? 20 : 25,
+  value: 2.5,
+  isHalf: true,
+};
+
+const Product = ({ product }) => {
+  return (
+    <Link className="productCard" to={product._id}>
+      <img src={product.images[0].url} alt={product.name} />
+      <p>{product.name}</p>
+
+      <div>
+        <ReactStars {...options} /> <span>(256 Reviews)</span>
+      </div>
+
+      <span>{product.price}</span>
+    </Link>
+  );
+};
+
+export default Product;
