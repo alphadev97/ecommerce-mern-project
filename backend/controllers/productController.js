73f8b308bd493c3b16c5e3ee051c71b58d4f1cd8
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-17 00:14:31 +0500

    redux

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -29,6 +29,7 @@ exports.getAllProducts = catchAsyncError(async (req, res) => {
   res.status(200).json({
     success: true,
     products,
+    productCount,
   });
 });
 
--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -5,3 +5,29 @@ import {
   ALL_PRODUCT_SUCCESS,
   CLEAR_ERRORS,
 } from "../constants/productConstants";
+
+export const getProduct = () => async (dispatch) => {
+  try {
+    dispatch({ type: ALL_PRODUCT_REQUEST });
+
+    const { data } = await axios.get("/api/v1/products");
+
+    dispatch({
+      type: ALL_PRODUCT_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: ALL_PRODUCT_FAIL,
+      payload: error.response.data.message,
+    });
+  }
+};
+
+// Clearing Errors
+
+export const clearErrors = () => async (dispatch) => {
+  dispatch({
+    type: CLEAR_ERRORS,
+  });
+};
