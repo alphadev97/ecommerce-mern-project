ab560d1f37268dc1cd70f7ed5469a57456a0dcc1
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-07-19 18:05:27 +0500

    productPagination

--- frontend/src/component/Product/Product.js
+++ frontend/src/component/Product/Product.js
@@ -1,18 +1,25 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import "./Product.scss";
 import { useSelector, useDispatch } from "react-redux";
 import { clearErrors, getProduct } from "../../actions/productAction";
 import Loader from "../layout/Loader/Loader";
 import ProductCard from "../Home/ProductCard";
 import { useParams } from "react-router-dom";
+import Pagination from "react-js-pagination";
 
 const Product = ({ match }) => {
   const dispatch = useDispatch();
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const { products, loading, error, productsCount, resultPerPage } =
+    useSelector((state) => state.products);
+
   const { keyword } = useParams();
 
-  const { products, loading, error, productsCount } = useSelector(
-    (state) => state.products
-  );
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
 
   useEffect(() => {
     dispatch(getProduct(keyword));
@@ -31,6 +38,22 @@ const Product = ({ match }) => {
               products.map((product) => (
                 <ProductCard key={product._id} product={product} />
               ))}
+          </div>
+          <div className="paginationBox">
+            <Pagination
+              activePage={currentPage}
+              itemsCountPerPage={resultPerPage}
+              totalItemsCount={productsCount}
+              onChange={setCurrentPageNo}
+              nextPageText="Next"
+              prevPageText="Prev"
+              firstPageText="1st"
+              lastPageText="Last"
+              itemClass="page-item"
+              linkClass="page-link"
+              activeClass="pageItemActive"
+              activeLinkClass="pageLinkActive"
+            />
           </div>
         </Fragment>
       )}
--- frontend/src/reducers/productReducer.js
+++ frontend/src/reducers/productReducer.js
@@ -20,6 +20,7 @@ export const productReducer = (state = { products: [] }, action) => {
         loading: false,
         products: action.payload.products,
         productsCount: action.payload.productsCount,
+        resultPerPage: action.payload.resultPerPage,
       };
     case ALL_PRODUCT_FAIL:
       return {
