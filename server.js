dde10b0cf03b2c6b971a5addc420bc6d67edc695
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-21 04:51:19 +0500

    mongoose

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 import express from "express";
 import colors from "colors";
+import dotenv from "dotenv";
+
+// Config .env
+dotenv.config();
 
 // Rest Object
-
 const app = express();
 
 // Rest API
-
 app.get("/", (req, res) => {
   res.send({
     message: "Welcome to our MERN STACK E-Commerce App",
@@ -14,11 +16,12 @@ app.get("/", (req, res) => {
 });
 
 // Port
-
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 // App Listen
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`.bgCyan.white);
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan
+      .white
+  );
 });
