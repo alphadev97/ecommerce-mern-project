22821ee871772e28718e5d0cd4b9634d54bf5da0
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-25 07:21:35 +0500

    PNF

--- client/src/pages/PageNotFound/PageNotFound.jsx
+++ client/src/pages/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import { Link } from "react-router-dom";
+import pnf from "../../assets/pnf.jpg";
+import "./PageNotFound.scss";
 
 const PageNotFound = () => {
   return (
     <Layout>
-      <h1>PageNotFound</h1>
+      <div className="pnf">
+        <h1>404</h1>
+        <img src={pnf} alt="" />
+        <h2>Looks Like You Are Lost</h2>
+        <Link to="/" className="pnf-btn">
+          Go Back
+        </Link>
+      </div>
     </Layout>
   );
 };
