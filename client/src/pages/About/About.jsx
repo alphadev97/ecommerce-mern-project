dd685b6a1ba1ed9074e88ab98e5336c5430b0862
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-27 03:34:32 +0500

    Policy, About

--- client/src/pages/About/About.jsx
+++ client/src/pages/About/About.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import contactImg from "../../assets/contact.jpg";
+import "./About.scss";
 
 const About = () => {
   return (
     <Layout>
-      <h1>About Page</h1>
+      <h1 className="ah1">About Us</h1>
+      <div className="about">
+        <div className="about-left">
+          <img src={contactImg} alt="" />
+        </div>
+
+        <div className="about-right">
+          <h2>About Alpha97 | E-Commerce</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing, elit sed <br />
+            venenatis varius sapien quam dignissim, leo tempus nostra velit{" "}
+            <br />
+            facilisis. Ad mi eros dapibus eget sagittis per enim bibendum <br />
+            rhoncus vehicula, mus fringilla cras gravida sollicitudin lectus{" "}
+            <br />
+            morbi convallis. Vivamus id nec tristique faucibus lacinia egestas{" "}
+            <br />
+            curabitur class aliquet convallis, varius arcu semper aenean dis non{" "}
+            <br />
+            nulla ultricies nullam, a sagittis sapien diam taciti nisl fringilla{" "}
+            <br />
+            massa sodales.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
--- client/src/pages/Policy/Policy.jsx
+++ client/src/pages/Policy/Policy.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import Layout from "../../components/Layout/Layout";
+import contactImg from "../../assets/contact.jpg";
+import "./Policy.scss";
 
 const Policy = () => {
   return (
     <Layout>
-      <h1>Policy</h1>
+      <h1 className="ph1">Policy</h1>
+      <div className="policy">
+        <div className="policy-left">
+          <img src={contactImg} alt="" />
+        </div>
+
+        <div className="policy-right">
+          <h2>Our Policy</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipiscing, elit sed <br />
+            venenatis varius sapien quam dignissim, leo tempus nostra velit
+            <br />
+            facilisis. Ad mi eros dapibus eget sagittis per enim bibendum <br />
+            rhoncus vehicula, mus fringilla cras gravida sollicitudin lectus
+            <br />
+            morbi convallis. Vivamus id nec tristique faucibus lacinia egestas
+            <br />
+            curabitur class aliquet convallis, varius arcu semper aenean dis non
+            <br />
+            nulla ultricies nullam, a sagittis sapien diam taciti nisl fringilla
+            <br />
+            massa sodales.
+          </p>
+        </div>
+      </div>
     </Layout>
   );
 };
