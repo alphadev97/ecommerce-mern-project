bf5b55933102c042bdc8f5a5ce79e175e70dde35
Muhammad Usama <muhammadusama30289@gmail.com>
2023-07-30 22:42:04 +0500

    Register Submit

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -1,23 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Register.scss";
 import Layout from "../../components/Layout/Layout";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [address, setAddress] = useState("");
+
+  // Form Function
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, phone, address);
+  };
+
   return (
     <Layout title={"Register - Alpha97 Ecommerce"}>
       <div className="register">
         <h1>Register Page</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="container">
-            <input type="text" placeholder="Enter Your Name" />
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
 
-            <input type="email" placeholder="Enter Your Email Address" />
+            <input
+              type="email"
+              placeholder="Enter Your Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
 
-            <input type="password" placeholder="Enter Your Password" />
+            <input
+              type="password"
+              placeholder="Enter Your Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
 
-            <input type="text" placeholder="Enter Your Phone No." />
+            <input
+              type="text"
+              placeholder="Enter Your Phone No."
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
 
-            <input type="text" placeholder="Enter Your Address" />
+            <input
+              type="text"
+              placeholder="Enter Your Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
 
             <button type="submit">Submit</button>
           </div>
