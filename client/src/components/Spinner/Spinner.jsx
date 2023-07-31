e5318f69378759129f889d688938b87d6107dd01
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-01 00:06:02 +0500

    Spinner

--- client/src/components/Spinner/Spinner.jsx
+++ client/src/components/Spinner/Spinner.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Spinner.scss";
 
 const Spinner = () => {
-  return <div className="loader">Loading...</div>;
+  const [count, setCount] = useState(5);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevValue) => --prevValue);
+    }, 1000);
+    count === 0 && navigate("/login");
+    return () => clearInterval(interval);
+  }, [count, navigate]);
+
+  return (
+    <>
+      <div className="body">
+        <div className="loader">Loading...</div>
+      </div>
+    </>
+  );
 };
 
 export default Spinner;
