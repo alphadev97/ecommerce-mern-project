59a06fbc90b895a2171ddac1238608f195d1dd1f
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-02 05:22:26 +0500

    Category

--- client/src/pages/AdminDashboard/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard/AdminDashboard.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import "./AdminDashboard.scss";
 import Layout from "../../components/Layout/Layout";
 import AdminMenu from "../../components/Layout/AdminMenu/AdminMenu";
+import { useAuth } from "../../context/auth";
 
 const AdminDashboard = () => {
+  const [auth] = useAuth();
+
   return (
     <Layout>
       <div className="container">
@@ -11,7 +14,13 @@ const AdminDashboard = () => {
           <div className="row-left">
             <AdminMenu />
           </div>
-          <div className="row-right">Content</div>
+          <div className="row-right">
+            <div className="card">
+              <h3>Admin Name: {auth?.user?.name}</h3>
+              <h3>Admin Email: {auth?.user?.email}</h3>
+              <h3>Admin Contact: {auth?.user?.phone}</h3>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
---
+++ client/src/pages/CreateCategory/CreateCategory.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CreateCategory.scss";
+
+const CreateCategory = () => {
+  return <div>CreateCategory</div>;
+};
+
+export default CreateCategory;
