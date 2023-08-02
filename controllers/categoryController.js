521340629924678ceef5dccee0bf0ebf0285fac9
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-02 22:45:28 +0500

    categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,9 +1,26 @@
+import categoryModel from "../models/categoryModel";
+import slugify from "slugify";
+
 export const createCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
     if (!name) {
       return res.status(401).send({ message: "Name is required" });
     }
+
+    const existingCategory = await categoryModel.findOne({ name });
+
+    if (existingCategory) {
+      return res.status(200).send({
+        success: true,
+        message: "Category Already Exists",
+      });
+    }
+
+    const category = await new categoryModel({
+      name,
+      slug: slugify(name),
+    }).save();
   } catch (error) {
     console.log(error);
     res.status(500).send({
