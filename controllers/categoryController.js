2d3bc8f3118aae889dbc660d5cf1436f811f427a
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-02 22:11:25 +0500

    categoryController

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,1 +1,15 @@
-export const createCategoryController = () => {};
+export const createCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({ message: "Name is required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Category",
+    });
+  }
+};
