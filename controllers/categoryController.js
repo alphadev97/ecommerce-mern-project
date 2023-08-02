6e64f25ecbec5b5a8c6071dc81bf15d9c23ec61f
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-03 00:50:31 +0500

    updateCategpry

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -35,3 +35,30 @@ export const createCategoryController = async (req, res) => {
     });
   }
 };
+
+// update category
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Category Updated Sucessfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while Updating Category",
+    });
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,15 +1,27 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
-import { createCategoryController } from "../controllers/categoryController.js";
+import {
+  createCategoryController,
+  updateCategoryController,
+} from "../controllers/categoryController.js";
 
 const router = express.Router();
 
 //routes
+// create category
 router.post(
   "/create-category",
   requireSignIn,
   isAdmin,
   createCategoryController
+);
+
+// update category
+router.put(
+  "/update-category",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
 );
 
 export default router;
