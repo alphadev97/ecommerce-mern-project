054cc765f50ee4008d5077000739f73bba13ca83
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-03 05:56:25 +0500

    CRUD Category Operations

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -62,3 +62,62 @@ export const updateCategoryController = async (req, res) => {
     });
   }
 };
+
+// Get all categories
+
+export const categoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.find();
+    res.status(200).send({
+      success: true,
+      message: "All categories list",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting all categories",
+    });
+  }
+};
+
+// Get single category
+
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get Single Category Successfull",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting single category",
+    });
+  }
+};
+
+// Delete category
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await categoryModel.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Category deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while deleting category",
+    });
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 import {
+  categoryController,
   createCategoryController,
+  deleteCategoryController,
+  singleCategoryController,
   updateCategoryController,
 } from "../controllers/categoryController.js";
 
@@ -18,10 +21,24 @@ router.post(
 
 // update category
 router.put(
-  "/update-category",
+  "/update-category/:id",
   requireSignIn,
   isAdmin,
   updateCategoryController
+);
+
+// get all category
+router.get("/get-category", categoryController);
+
+// single cartegory
+router.get("/single-category/:slug", singleCategoryController);
+
+// delet category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  deleteCategoryController
 );
 
 export default router;
