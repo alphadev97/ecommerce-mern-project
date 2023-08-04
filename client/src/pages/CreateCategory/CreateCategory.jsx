9abe79f1058ccecd67cfc2adfdaf8f883bc8bd6d
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-05 00:13:39 +0500

    updateCategory

--- client/src/pages/CreateCategory/CreateCategory.jsx
+++ client/src/pages/CreateCategory/CreateCategory.jsx
@@ -5,10 +5,14 @@ import AdminMenu from "../../components/Layout/AdminMenu/AdminMenu";
 import toast from "react-hot-toast";
 import axios from "axios";
 import CategoryForm from "../../components/Form/CategoryForm";
+import { Modal } from "antd";
 
 const CreateCategory = () => {
   const [categories, setCategories] = useState([]);
   const [name, setName] = useState("");
+  const [visible, setVisible] = useState(false);
+  const [selected, setSelected] = useState(null);
+  const [updatedName, setUpdatedName] = useState("");
 
   // handle form
   const handleSubmit = async (e) => {
@@ -49,6 +53,29 @@ const CreateCategory = () => {
     getAllCategory();
   }, []);
 
+  // update category
+
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put(
+        `http://localhost:8080/api/v1/category/update-category/${selected._id}`,
+        { name: updatedName }
+      );
+      if (data.success) {
+        toast.success(`${updatedName} is updated!`);
+        setSelected(null);
+        setUpdatedName("");
+        setVisible(false);
+        getAllCategory();
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
+
   return (
     <Layout title={"Dashboard - Create Category - Alpha97 ECommerce"}>
       <div className="container">
@@ -59,7 +86,7 @@ const CreateCategory = () => {
           <div className="row-right">
             <div className="card">
               <h1>Create Category</h1>
-              <div>
+              <div className="form">
                 <CategoryForm
                   handleSubmit={handleSubmit}
                   value={name}
@@ -82,7 +109,16 @@ const CreateCategory = () => {
                           <tr>
                             <td key={c._id}>{c.name}</td>
                             <td>
-                              <button className="btn">Edit</button>
+                              <button
+                                className="btn"
+                                onClick={() => {
+                                  setVisible(true);
+                                  setUpdatedName(c.name);
+                                  setSelected(c);
+                                }}
+                              >
+                                Edit
+                              </button>
                               <button className="btn-danger">Delete</button>
                             </td>
                           </tr>
@@ -92,6 +128,17 @@ const CreateCategory = () => {
                   </table>
                 </div>
               </div>
+              <Modal
+                onCancel={() => setVisible(false)}
+                footer={null}
+                visible={visible}
+              >
+                <CategoryForm
+                  value={updatedName}
+                  setValue={setUpdatedName}
+                  handleSubmit={handleUpdate}
+                />
+              </Modal>
             </div>
           </div>
         </div>
