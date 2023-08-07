221e07337f07af41933cad89b66ad8449d69a362
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-07 22:28:11 +0500

    Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,16 +2,33 @@ import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import { useAuth } from "../../context/auth";
 import "./Home.scss";
+import axios from "axios";
 
 const Home = () => {
   const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+
+  // Get all products
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/product/get-product"
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
   return (
     <Layout title={"All Products - Best Offers - Alpha97 E-Commerce"}>
       <div className="home">
         <div className="left">
-          <h6>Filter By Category</h6>
+          <h4>Filter By Category</h4>
         </div>
         <div className="right">
           <h1>All Products</h1>
