2222247dc521e93060f06c1e9a3b4520f99b6dd3
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-08 00:00:22 +0500

    Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,43 @@
 import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
-import { useAuth } from "../../context/auth";
 import "./Home.scss";
 import axios from "axios";
+import { Checkbox } from "antd";
 
 const Home = () => {
-  const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [checked, setChecked] = useState([]);
+
+  // get all categories
+  const getAllCategory = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/category/get-category"
+      );
+      if (data?.success) {
+        setCategories(data?.category);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Filter by category
+
+  const handleFilter = (value, id) => {
+    let all = [...checked];
+    if (value) {
+      all.push(id);
+    } else {
+      all = all.filter((c) => c !== id);
+    }
+    setChecked(all);
+  };
+
+  useEffect(() => {
+    getAllCategory();
+  }, []);
 
   // Get all products
   const getAllProducts = async () => {
@@ -29,11 +59,37 @@ const Home = () => {
       <div className="home">
         <div className="left">
           <h4>Filter By Category</h4>
+          <div className="cat-filter">
+            {categories?.map((c) => (
+              <Checkbox
+                key={c._id}
+                onChange={(e) => handleFilter(e.target.checked, c._id)}
+              >
+                {c.name}
+              </Checkbox>
+            ))}
+          </div>
         </div>
         <div className="right">
+          {JSON.stringify(checked, null, 4)}
           <h1>All Products</h1>
-          <div className="products">
-            <h1>products</h1>
+          <div className="card-container">
+            {products?.map((p) => (
+              <div className="card" key={p._id}>
+                <img
+                  src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                  alt={p.name}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{p.name}</h5>
+                  <p className="card-text">{p.description}</p>
+                </div>
+                <div className="btn">
+                  <button>More Details</button>
+                  <button>Add To Cart</button>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
