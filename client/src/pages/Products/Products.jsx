ed87607b4d5033869e19946771321a0667d13444
Muhammad Usama <muhammadusama30289@gmail.com>
2023-08-07 05:15:36 +0500

    Products

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Products.scss";
 import AdminMenu from "../../components/Layout/AdminMenu/AdminMenu";
 import Layout from "../../components/Layout/Layout";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  // Get all products
+
+  const getAllProducts = async () => {
+    try {
+      const { data } = await axios.get(
+        "http://localhost:8080/api/v1/product/get-product"
+      );
+      setProducts(data.products);
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+
+  // lifecycle method
+
+  useEffect(() => {
+    getAllProducts();
+  }, []);
+
   return (
     <Layout>
       <div className="products_container">
@@ -13,6 +38,25 @@ const Products = () => {
           </div>
           <div className="row-right">
             <h1>All Products List</h1>
+            <div className="card-container">
+              {products?.map((p) => (
+                <Link
+                  to={`/dashboard/admin/product/${p.slug}`}
+                  className="product-link"
+                >
+                  <div className="card" key={p._id}>
+                    <img
+                      src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                      alt={p.name}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{p.name}</h5>
+                      <p className="card-text">{p.description}</p>
+                    </div>
+                  </div>
+                </Link>
+              ))}
+            </div>
           </div>
         </div>
       </div>
