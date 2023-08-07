d408f7c09e03649c95db1f333d0d8a17607fa369
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-07 21:34:17 +0500

    Home

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../../components/Layout/Layout";
 import { useAuth } from "../../context/auth";
+import "./Home.scss";
 
 const Home = () => {
   const [auth, setAuth] = useAuth();
+  const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
   return (
-    <Layout>
-      <h1>Home Page</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+    <Layout title={"All Products - Best Offers - Alpha97 E-Commerce"}>
+      <div className="home">
+        <div className="left">
+          <h6>Filter By Category</h6>
+        </div>
+        <div className="right">
+          <h1>All Products</h1>
+          <div className="products">
+            <h1>products</h1>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
