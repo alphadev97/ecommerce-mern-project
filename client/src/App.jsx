48e6fd4ea2bf46e3a8911011706b268c8a5795a1
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-09 01:35:01 +0500

    CategoryProduct

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,6 +21,7 @@ import UpdateProduct from "./pages/UpdateProduct/UpdateProduct";
 import Search from "./pages/Search/Search";
 import ProductDetails from "./pages/ProductDetails.jsx/ProductDetails";
 import Categories from "./pages/Categories/Categories";
+import CategoryProduct from "./pages/CategoryProduct/CategoryProduct";
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/product/:slug" element={<ProductDetails />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="/category/:slug" element={<CategoryProduct />} />
         <Route path="/search" element={<Search />} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="user" element={<Dashboard />} />
---
+++ client/src/pages/CategoryProduct/CategoryProduct.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./CategoryProduct.scss";
+import Layout from "../../components/Layout/Layout";
+
+const CategoryProduct = () => {
+  return (
+    <Layout>
+      <div className="container">
+        <h1>Cat Name</h1>
+      </div>
+    </Layout>
+  );
+};
+
+export default CategoryProduct;
