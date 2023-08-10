8f29aa2c64f681126f0d16f51caaabea68498dd8
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-11 01:21:23 +0500

    PaymentGateway

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,6 +2,15 @@ import slugify from "slugify";
 import productModel from "../models/productModel.js";
 import fs from "fs";
 import categoryModel from "./../models/categoryModel.js";
+import braintree from "braintree";
+
+// Payment gateway
+var gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 export const createProductController = async (req, res) => {
   try {
