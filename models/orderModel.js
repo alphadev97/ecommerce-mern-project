c19a271f1bb8b18e24e04c973e1765532fcf2323
Muhammad Usama <116688082+usamaweb@users.noreply.github.com>
2023-08-11 17:23:31 +0500

    orderModel

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,5 +1,25 @@
 import mongoose, { mongo } from "mongoose";
 
-const orderSchema = new mongoose.Schema({});
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Product",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "User",
+    },
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "Delievered", "Cancel"],
+    },
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Order", orderSchema);
